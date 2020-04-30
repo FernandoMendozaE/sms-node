78d9aab50386aa94cd4217c229c0335611d0e26b
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2020-04-30 18:30:04 -0400

    Ordenando las rutas

---
+++ src/controllers/index.controller.js
@@ -0,0 +1,39 @@
+const { sendMessage } = require('../twilio/send-sms')
+const SMS = require('../models/sms')
+
+/**
+ * Método encargado de renderizar
+ */
+const indexController = async (req, res) => {
+  const messages = await SMS.find().lean() // TODO: lean: convierte un objeto mongo a javascript
+  console.log('typeof', typeof messages)
+  // messages.forEach(m => {
+  //   console.log(m.body)
+  // })
+  res.render('index', { messages }) // TODO: renderizando en express y enviando datos
+}
+
+/**
+ * Método encargado de enviar el mensaje
+ */
+const postMenssage = async (req, res) => {
+  // console.log(req.body)
+  const { message, phone } = req.body
+
+  if (!message || !phone) return res.json('Missing message or phone.')
+
+  const result = await sendMessage(message, phone)
+  console.log(result.sid)
+
+  await SMS.create({ Body: req.body.message, To: req.body.phone })
+
+  res.redirect('/') // TODO: redireccionar en express
+}
+
+const reciveMessage = async (req, res) => {}
+
+module.exports = {
+  indexController,
+  postMenssage,
+  reciveMessage
+}
