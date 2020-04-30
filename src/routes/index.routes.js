225a56587bd7f0d2db384a506b1b680b678dfccd
Fernando Trabajo_Yotta <feryotta2@gmail.com>
2020-04-30 18:30:04 -0400

    Oredenando las rutas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,30 +1,15 @@
 const { Router } = require('express')
 const router = Router()
 
-const { sendMessage } = require('../twilio/send-sms')
-const SMS = require('../models/sms')
+const { indexController, postMenssage, reciveMessage } = require('../controllers/index.controller') // TODO: importando funciones de las rutas
 
-router.get('/', async (req, res) => {
-  const messages = await SMS.find().lean() // TODO: lean: convierte un objeto mongo a javascript
-  console.log('typeof', typeof(messages))
-  // messages.forEach(m => {
-  //   console.log(m.body)
-  // })
-  res.render('index', { messages }) // TODO: renderizando en express y enviando datos
-})
+// Main Routes
+router.get('/', indexController)
 
-router.post('/send-sms', async (req, res) => {
-  // console.log(req.body)
-  const { message, phone } = req.body
+// Send an SMS
+router.post('/send-sms', postMenssage)
 
-  if (!message || !phone) return res.json('Missing message or phone.')
-
-  const result = await sendMessage(message, phone)
-  console.log(result.sid)
-
-  await SMS.create({ Body: req.body.message, To: req.body.phone })
-
-  res.redirect('/') // TODO: redireccionar en express
-})
+// Receive an SMS
+router.post('/sms', reciveMessage)
 
 module.exports = router
